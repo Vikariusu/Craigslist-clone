61a3dcc922d32fd77d0371270299079891cb2f1a
Weronika Tomaszewska <vikariusu@gmail.com>
2019-08-05 21:29:30 -0400

    Fix heroku port issues

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.listen(7777, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("~ Starting the server ~");
 });
